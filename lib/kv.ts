4723e7b660cf3a6aee73f49494b2af6475b0ff75
SK8100 <123937187+SK8100@users.noreply.github.com>
2025-12-30 17:31:55 +0530

    Update Redis configuration to use new environment variables

--- lib/kv.ts
+++ lib/kv.ts
@@ -1,6 +1,4 @@
-import { Redis } from "@upstash/redis";
-
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.REDIS_URL!,
+  token: process.env.REDIS_TOKEN!,
 });
